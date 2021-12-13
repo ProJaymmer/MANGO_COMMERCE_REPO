58add81df69a94d8140c5a25222f68aa3eb6f241
Jay Nguyen <jay@Jays-iMac.local>
2021-12-13 14:57:55 -0800

    Add user serializer for sign up route

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -7,3 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+        # This create method will be used for model creation
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
